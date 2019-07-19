947755ebf5614145b6162666c3b4ca83e58b6771
Bryan Szendel <bryan.szendel@gmail.com>
2019-07-19 14:07:20 -0600

    destructured props passed into JediCard

--- starwars/src/components/JediCard.js
+++ starwars/src/components/JediCard.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Card } from 'semantic-ui-react'
 
-export default function JediCard(props) {
+export default function JediCard({num}) {
     return (
         <Card style={{ maxWidth: '250px', height: '150px', margin: '1rem' }}>
             <Card.Content>
-                <Card.Header>{props.num.name}</Card.Header>
+                <Card.Header>{num.name}</Card.Header>
                 <Card.Description>
-                    Birth Year: {props.num.birth_year}<br/>
-                    Gender: {props.num.gender}<br/>
-                    Eye Color: {props.num.eye_color}<br/>
-                    Height: {props.num.height}
+                    Birth Year: {num.birth_year}<br/>
+                    Gender: {num.gender}<br/>
+                    Eye Color: {num.eye_color}<br/>
+                    Height: {num.height}
                 </Card.Description>
             </Card.Content>
         </Card>
